959a46af3c96cd2f504166f999f132974cca46ff
N Fourtounis <nickfortune@windowslive.com>
2018-03-30 15:49:18 +0300

    comments

--- src/exercise1.py
+++ src/exercise1.py
@@ -1,8 +1,8 @@
 __author__ = "NFourtounis"
 __date__ = "$Mar 30, 2018 7:26:32 PM$"
 
-def isInt(character):
-    try:
+def isInt(character):#Function που ελέγχει αν ένας χαρακτήρας είναι int
+    try:#Αν παίζει το try χωρίς να πετάξει error η int() πάει να πει ότι ο χαρακτήρας είναι int
         int(character)
         return True
     except ValueError:
@@ -10,14 +10,12 @@ def isInt(character):
 
 
 if __name__ == "__main__":
-    #str1=input('Enter a string:')
-    str1="3t4m6i3ry2j"
+    str1=input('Enter a string:')#Ζητάω από τον χρήστη ένα string
     str2=""
-    print(len(str1))
-    for i in range(0,len(str1),1):  
-        if(isInt(str1[i])):
-            #print(str1[i+1]*int(str1[i]))
-            str2+=str1[i+1]*(int(str1[i])-1)
-        else:
+    for i in range(0,len(str1),1):#Περνάω κάθε χαρακτήρα του string 
+        if(isInt(str1[i])):# Αν βρω integer
+            str2+=str1[i+1]*(int(str1[i])-1)#προσθέτω στο δεύτερο string, τον χαρακτήρα μετά τον int
+                                            #έτσι ώστε να προσθέσω τόσα γράμματα όσο το νούμερο
+        else:#αλλιως προσθέτω στο δεύτερο string, το μοναδικό γράμμα που βρήκα
             str2+=str1[i]
-    print(str2)
+    print(str2)#Εκτυπώνω το string
